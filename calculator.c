dfe81607995dc0ebaceb77c15d09a08688c808c5
Swapnil-Srivastava273 <ss.bluescreenofd273@gmail.com>
2022-07-27 13:20:45 +0530

    More inputs + bugfix

--- calculator.c
+++ calculator.c
@@ -2,9 +2,11 @@
 int main(){
     double a,b;
     printf("Enter first number: ");
-    scanf("%lf",a);
+    scanf("%lf",&a);
     printf("Enter second number: ");
-    scanf("%lf",b);
-    
+    scanf("%lf",&b);
+    printf("What operation do you want to perform?\nEnter a number:\n\n1) Addition\n2)Subtraction\n3)Multiplication\n4)Division\n\nEnter your choice: ");
+    int n;
+    scanf("%d",&n);
     return 0;
 }
