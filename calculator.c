4f9b6882f7f903fd28813a44f82041f8e9fa81eb
Swapnil-Srivastava273 <ss.bluescreenofd273@gmail.com>
2022-07-27 13:17:38 +0530

    Input numbers

--- calculator.c
+++ calculator.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(){
-
+    double a,b;
+    printf("Enter first number: ");
+    scanf("%lf",a);
+    printf("Enter second number: ");
+    scanf("%lf",b);
+    
     return 0;
 }
