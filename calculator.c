90eb42dc2dab562be9c9629dbdab5054a470d561
Swapnil-Srivastava273 <ss.bluescreenofd273@gmail.com>
2022-07-27 13:23:15 +0530

    Addition added

--- calculator.c
+++ calculator.c
@@ -8,5 +8,10 @@ int main(){
     printf("What operation do you want to perform?\nEnter a number:\n\n1) Addition\n2)Subtraction\n3)Multiplication\n4)Division\n\nEnter your choice: ");
     int n;
     scanf("%d",&n);
+    switch(n){
+        case 1:
+            printf("Addition is : %lf",a+b);
+            break;
+    }
     return 0;
 }
