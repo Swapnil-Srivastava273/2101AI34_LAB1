fd0bc7c4dfe98afca6488a2dcec81adefa14c232
Swapnil-Srivastava273 <ss.bluescreenofd273@gmail.com>
2022-07-27 14:33:12 +0530

    Added merge sort

--- sorting.c
+++ sorting.c
@@ -28,7 +28,46 @@ void selection_sort(int arr[],int n){
 void bubble_sort(int arr[],int n){
     int i,j;
     for(i=0;i<n-1;i++){
-        for(j=0;j<n-i-1;j++)if(arr[j]>arr[j+1])swap(&arr[j],arr[j+1]);
+        for(j=0;j<n-i-1;j++)if(arr[j]>arr[j+1])swap(&arr[j],&arr[j+1]);
+    }
+}
+void merge(int arr[],int l,int m,int r){
+    int i,j,k;
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1],R[n2];
+    for(i=0;i<n1;i++)L[i]=arr[l+i];
+    for(j=0;j<n2;j++)R[j]=arr[m+1+j];
+    i=0;
+    j=0;
+    k=l;
+    while(i<n1&&j<n2){
+        if(L[i]<=R[j]){
+            arr[k]=L[i];
+            i++;
+        }else{
+            arr[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k]=L[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        arr[k]=R[j];
+        j++;
+        k++;
+    }
+}
+void merge_sort(int arr[],int l,int r){
+    if(l<r){
+        int mid=l+(r-l)/2;
+        merge_sort(arr,l,mid);
+        merge_sort(arr,mid+1,r);
+        merge(arr,l,mid,r);
     }
 }
 int main(){
@@ -38,7 +77,7 @@ int main(){
     int arr[n];
     printf("Enter array items (space seperated): ");
     for(int i=0;i<n;i++)scanf("%d",&arr[i]);
-    insertion_sort(arr,n);
+    bubble_sort(arr,n);
     printf("Array after sort: ");
     for(int i=0;i<n;i++)printf("%d ",arr[i]);
     return 0;
